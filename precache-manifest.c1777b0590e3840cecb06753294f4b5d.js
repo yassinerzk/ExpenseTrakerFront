9b7eaf0626e3c183a36841b07170a411ce7747d5
Yassine Razzouki <hello@yassinerazzouki.com>
2020-02-25 04:08:42 +0100

    Updates

---
+++ precache-manifest.c1777b0590e3840cecb06753294f4b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7737c85ba24cf6abf5b5536b63862565",
+    "url": "/ExpenseTrakerFront/index.html"
+  },
+  {
+    "revision": "ad726dded117aea6204a",
+    "url": "/ExpenseTrakerFront/static/css/main.1515c12b.chunk.css"
+  },
+  {
+    "revision": "6c4a503a07e7c39835e6",
+    "url": "/ExpenseTrakerFront/static/js/2.fe0fce3d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ExpenseTrakerFront/static/js/2.fe0fce3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad726dded117aea6204a",
+    "url": "/ExpenseTrakerFront/static/js/main.4bd45cc3.chunk.js"
+  },
+  {
+    "revision": "048bcebe47b7bc866931",
+    "url": "/ExpenseTrakerFront/static/js/runtime-main.3d2b59e1.js"
+  }
+]);
